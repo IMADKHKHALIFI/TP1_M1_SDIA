84393cd1b760989663e7283ecb5067428c333862
IMADKHKHALIFI <imad.khalyfy@gmail.com>
2025-04-17 23:23:06 +0100

    Premiere commit , creation de package Dao

--- src/main/java/com/exemple/dao/DaoImpli.java
+++ src/main/java/com/exemple/dao/DaoImpli.java
@@ -4,9 +4,7 @@ public class DaoImpli  implements IDao {
 
     @Override
     public double getData() {
-        System.out.println("Version Base de données");
-        double t = 34;
-        return t;
+
 
     }
 }
